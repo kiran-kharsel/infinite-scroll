5fdf646c2030028612174c2e85eca9c5f4adda0f
kiran-kharsel <kirankharsel@gmail.com>
2026-04-13 12:03:18 +0530

    feat: calculate remainingScroll

--- src/infinite-scroll/InfiniteScroll.jsx
+++ src/infinite-scroll/InfiniteScroll.jsx
@@ -5,8 +5,21 @@ function InfiniteScroll() {
   const [data, setData] = useState([... new Array(40)])
 
 
+  function loadMore(){
+
+  }
+
+  function handleScroll(e){
+    console.log('scrolling')
+    const scrollTop = e.target.scrollTop;
+    const scrollHeight = e.target.scrollHeight;
+    const clientHeight = e.target.clientHeight;
+
+    console.log(scrollTop)
+  }
+
   return (
-    <div className='scroll-container'>
+    <div onScroll={handleScroll} className='scroll-container'>
       {
         data.map((row, index) => {
           return(
