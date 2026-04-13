b3777062ac93d39e96dc36e6d746b2ae4744dda7
kiran-kharsel <kirankharsel@gmail.com>
2026-04-13 11:55:00 +0530

    feat: add data to scroll-container

--- src/infinite-scroll/InfiniteScroll.jsx
+++ src/infinite-scroll/InfiniteScroll.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 
 function InfiniteScroll() {
+  const [data, setData] = useState([... new Array(40)])
+
+
   return (
     <div className='scroll-container'>
-      container
+      {
+        data.map((row, index) => {
+          return(
+            <div className='row-data' key={index}>{index + 1}</div>
+          )
+        })
+      }
     </div>
   )
 }
