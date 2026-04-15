216170a861efb0332602553b68d94ad789613259
kiran-kharsel <kirankharsel@gmail.com>
2026-04-15 06:28:20 +0530

    feat: implement infinite scroll with IntersectionObserver

--- src/infinite-scroll/InfiniteScroll.jsx
+++ src/infinite-scroll/InfiniteScroll.jsx
@@ -7,21 +7,22 @@ import Post from './post/Post';
 function InfiniteScroll() {
   const [data, setData] = useState([])
   const [loading, setLoading] = useState(false)
+  const [page, setPage] = useState(1)
 
 
   useEffect(()=>{
-    fetch('https://picsum.photos/v2/list?page=1&limit=4')
+    fetch(`https://picsum.photos/v2/list?page=${page}&limit=3`)
     .then((res) => {
       return res.json()
     })
     .then((fetchedData) => {
       setData(prev => [...prev, ...fetchedData])
     })
-  },[])
+  },[page])
 
   return (
     <div className='scroll-container'>
-      <Post data={data}/>
+      <Post data={data} setPage={setPage}/>
     </div>
   )
 }
--- src/infinite-scroll/post/Post.jsx
+++ src/infinite-scroll/post/Post.jsx
@@ -1,20 +1,47 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './style.css'
 
-function Post({data}) {
-  return (
-    <div className='post-container'>
-        {
-            data.map((item) => {
-                return (
-                    <div className='post' key={item.id}>
-                        <img className='post-image' src={item.download_url} alt='post-image' />
-                    </div>
-                )
-            })
+function Post({ data, setPage }) {
+
+    useEffect(() => {
+
+        const callback = (entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    // loadmore
+                    setPage(prev => prev + 1)
+                }
+            });
         }
-    </div>
-  )
+
+        const options = {
+            threshold: 0.5,
+        }
+
+
+        const observer = new IntersectionObserver(callback, options)
+        const target = document.querySelector('.post:last-child')
+        if(target){
+            observer.observe(target);
+        }
+
+        return () => observer.disconnect();
+
+    }, [data.length])
+
+    return (
+        <div className='post-container'>
+            {
+                data.map((item) => {
+                    return (
+                        <div className='post' key={item.id}>
+                            <img className='post-image' src={item.download_url} alt='post-image' />
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
 }
 
 export default Post
